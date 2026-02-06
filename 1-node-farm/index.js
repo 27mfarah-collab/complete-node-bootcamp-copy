cc1edb276be70d1db5489b92383db0c7ba3f0729
Magan Farah <27mfarah@cpsd.us>
2026-02-06 13:09:45 -0500

    filling out  the templates and making my readme file

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -31,19 +31,37 @@ const url = require('url');
 
 /////////////////////////////////
 // SERVER 
+const replaceTemplate = (temp, product) => {
+    let output =  temp.replace(/{%!PRODUCTNAME!%}/g, product.productName);
+}
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,  'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,  'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,  'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8',);
 const data0bj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
+
+
         res.end('This is the OVERVIEW');
+
+    // Product page    
     } else if (pathName === '/PRODUCT') {
         res.end('This is the PRODUCT');
+
+    // API
     } else if (pathName === '/api') {
         res.writeHead(200, { 'Content-type': 'application/json'});
         res.end(data);
+    
+    //  Not found
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
